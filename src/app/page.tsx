af5ff32f3feeacaf04407c74d3032175e13b7183
Shoury Sharma <shanuu12e@gmail.com>
2023-11-05 14:24:14 +0530

    fix: home page designs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 import TypewriteTitle from '@/components/custom/typewriter-title'
 import { Button } from '@/components/ui/button'
 import { ArrowRight } from 'lucide-react'
-import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className='bg-gradient-to-r min-h-screen from-rose-100 to-teal-100'>
+    <div className=''>
       <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
         <h1 className="font-semibold text-7xl text-center">
           AI <span className='text-indigo-600 font-bold'>note taking</span>{' '} assistant
         </h1>
         <div className="mt-4"> </div>
-        <h2 className='font-semibold text-slate-700 text-3xl text-center'><TypewriteTitle /></h2>
+        <h2 className='font-semibold text-3xl text-center'><TypewriteTitle /></h2>
         <div className="mt-8"> </div>
         <div className='flex justify-center'>
 
